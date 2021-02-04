aeb837516d5fb3869c36f10e83b1f61205354345
Ahsan Ullah <ahsanullahsunsbd@gmail.com>
2021-02-04 09:05:31 +0600

    first commit

---
+++ weather.js
@@ -0,0 +1,40 @@
+let apiLink = "http://api.openweathermap.org/data/2.5/weather";
+let apiKey = 'd615e9df6db970486f02a6c42431f0d8';
+
+
+let link = function (city) {
+   
+    let url = `${apiLink}?q=${city}&units=metric&appid=${apiKey}`;
+
+    fetch(url)
+		.then((response) => response.json())
+        .then((data) => {
+            document.getElementById("weather-status").style.display = "block";
+            document.getElementById('cityName').innerText = data.name;
+            document.getElementById("temp").innerText =
+			data.main.feels_like;
+            document.getElementById("clouds").innerText =
+			data.clouds.all;
+            document.getElementById("wind").innerText =
+			data.wind.speed;
+            document.getElementById("weather").innerText =
+			data.weather[0].description;
+
+   
+			console.log(data);
+		});
+     
+}
+
+
+
+function ClickSearch() {
+
+    const citySearch = document.getElementById("Search-city").value;
+    
+    document.getElementById(" header-text ").style.display = "none";
+    link(citySearch);
+    
+
+    
+}
